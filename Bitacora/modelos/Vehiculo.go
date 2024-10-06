b1aa6d6b661ee56f83883f779bc7c8d0f3a3f4e8
Melina Britos <melinal.britos@gmail.com>
2024-10-05 22:09:17 -0300

    Primeros endpoints vehiculo

---
+++ Bitacora/modelos/Vehiculo.go
@@ -0,0 +1,18 @@
+package modelos
+
+import (
+	"gorm.io/gorm"
+)
+
+type Vehiculo struct {
+	gorm.Model
+
+	Matricula                 string  `gorm:"unique;not null"`
+	Marca                     string  `gorm:"not null"`
+	Modelo                    string  `gorm:"not null"`
+	FechaIngreso              string  `gorm:"not null"`
+	Estado                    string  `gorm:"not null"`
+	PesoAdmitido              float32 `gorm:"not null"`
+	VolumenAdmitidoMtsCubicos float32 `gorm:"not null"`
+	KmRecorridos              int
+}
---
+++ Bitacora/rutas/Vehiculo.rutas.go
@@ -0,0 +1,70 @@
+package rutas
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/MelinaBritos/TP-Principal-AMAZONA/Bitacora/modelos"
+	"github.com/MelinaBritos/TP-Principal-AMAZONA/baseDeDatos"
+	"github.com/gorilla/mux"
+)
+
+func GetVehiculosHandler(w http.ResponseWriter, r *http.Request) {
+	var vehiculos []modelos.Vehiculo
+
+	baseDeDatos.DB.Find(&vehiculos)
+	json.NewEncoder(w).Encode(&vehiculos)
+}
+
+func GetVehiculoHandler(w http.ResponseWriter, r *http.Request) {
+	var vehiculo modelos.Vehiculo
+	parametros := mux.Vars(r)
+
+	baseDeDatos.DB.First(&vehiculo, parametros["id"])
+
+	if vehiculo.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) // error 404
+		w.Write([]byte("Producto no encontrado"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&vehiculo)
+
+}
+
+func PostProductosHandler(w http.ResponseWriter, r *http.Request) {
+	var vehiculos []modelos.Vehiculo
+
+	if err := json.NewDecoder(r.Body).Decode(&vehiculos); err != nil {
+		http.Error(w, "Error al decodificar los vehiculos: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for _, vehiculo := range vehiculos {
+		if err := validarVehiculo(vehiculo); err != nil {
+			http.Error(w, "Vehiculo inválido: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	tx := baseDeDatos.DB.Begin()
+	for _, vehiculo := range vehiculos {
+		vehiculoCreado := tx.Create(&vehiculo)
+
+		err := vehiculoCreado.Error
+		if err != nil {
+			tx.Rollback()
+			http.Error(w, "Error al crear los vehiculos: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	tx.Commit()
+	w.WriteHeader(http.StatusOK)
+}
+
+func validarVehiculo(vehiculo modelos.Vehiculo) error {
+
+	// how??
+	return nil
+}
