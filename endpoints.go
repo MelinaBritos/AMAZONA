be9d5c304676aa010f6490aeda0d6a5c8fa5bc8e
Miguel Angel Gabrielli <154911627+Robotitusx@users.noreply.github.com>
2024-10-09 22:25:27 -0300

    agregados los endpoints (#11)
    * Update main.go
    
    * manejo de endpoints

--- endpoints.go
+++ endpoints.go
@@ -5,25 +5,49 @@ import (
 
 	"github.com/MelinaBritos/TP-Principal-AMAZONA/Bitacora/rutas"
 	"github.com/MelinaBritos/TP-Principal-AMAZONA/Proveedor/rutasProveedor"
+	
+	rutasUsuarios "github.com/MelinaBritos/TP-Principal-AMAZONA/Usuarios/rutasUsuarios"
 	"github.com/gorilla/mux"
 )
 
-func Iniciar() {
+
+func GenerarEndpoints() {
 
 	r := mux.NewRouter()
 
+	EndpointsVehiculo(r)
+	EndpointsProveedores(r)
+	EndpointsUsuarios(r)
+
+	http.ListenAndServe(":3000", r)
+}
+
+func EndpointsVehiculo(r *mux.Router)  {
 	//HANDLERS VEHICULO
 	r.HandleFunc("/vehiculos", rutas.GetVehiculosHandler).Methods("GET")
 	r.HandleFunc("/vehiculos/{id}", rutas.GetVehiculoHandler).Methods("GET")
 	r.HandleFunc("/vehiculos", rutas.PostVehiculoHandler).Methods("POST")
+}
 
+func EndpointsProveedores(r *mux.Router)  {
 	// HANDLERS PROOVEDOR
 	r.HandleFunc("/proveedor/{id_proveedor}", rutasProveedor.GetProveedorHandler).Methods("GET")
 	r.HandleFunc("/proveedor", rutasProveedor.GetProveedoresHandler).Methods("GET")
 	r.HandleFunc("/proveedor", rutasProveedor.PutProveedorHandler).Methods("PUT")   //Modificar datos de algun usuario
 	r.HandleFunc("/proveedor", rutasProveedor.PostProveedorHandler).Methods("POST") //crear un usuario
 
-	//HANDLERS USUARIOS
+}
 
-	http.ListenAndServe(":3000", r)
+func EndpointsUsuarios(r *mux.Router){
+
+	r.HandleFunc("/usuarios", rutasUsuarios.GetUsuariosHandler).Methods("GET")
+    r.HandleFunc("/usuarios/{username}", rutasUsuarios.GetUsuarioByIdHandler).Methods("GET")
+	r.HandleFunc("/usuarios/roles/{rol}", rutasUsuarios.GetUsuariosByRolHandler).Methods("GET")
+	
+	r.HandleFunc("/usuarios/{username}", rutasUsuarios.EditarUsuario).Methods("PUT") //Modificar datos de algun usuario
+	r.HandleFunc("/usuarios", rutasUsuarios.CrearUsuario).Methods("POST") //crear un usuario
+	r.HandleFunc("/usuarios/login", rutasUsuarios.Loguearse).Methods("POST")
+
+	r.HandleFunc("/usuarios/{username}",rutasUsuarios.EliminarUsuario).Methods("DELETE")
+	
 }
