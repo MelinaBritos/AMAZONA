98ff8fab94fa92994b952bed9358062d8db8adf0
Miguel Angel Gabrielli <154911627+Robotitusx@users.noreply.github.com>
2024-10-11 10:22:39 -0300

    agregado un merge con lo que hizo Melina (#14)
    * Update main.go
    
    * manejo de endpoints
    
    * puerto agregado
    
    * cambios para pruebas
    
    * agregados
    
    * remerge de cambios

--- endpoints.go
+++ endpoints.go
@@ -1,27 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/MelinaBritos/TP-Principal-AMAZONA/Bitacora/rutas"
 	"github.com/MelinaBritos/TP-Principal-AMAZONA/Proveedor/rutasProveedor"
+	"github.com/joho/godotenv"
 
 	rutasUsuarios "github.com/MelinaBritos/TP-Principal-AMAZONA/Usuarios/rutasUsuarios"
 	"github.com/gorilla/mux"
 )
 
+
 func GenerarEndpoints() {
 
 	r := mux.NewRouter()
+
+
+	port, err := CargarPuerto()
+
+	if err != nil {
+		println(err)
+	}
 
 	EndpointsVehiculo(r)
 	EndpointsProveedores(r)
 	EndpointsUsuarios(r)
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":" + port, r)
 }
 
-func EndpointsVehiculo(r *mux.Router) {
+func CargarPuerto() (string, error){
+
+	err := godotenv.Load(".env.example")
+	if err != nil {
+		return os.Getenv("PORT"), err
+	}
+	return os.Getenv("PORT"), nil
+}
+
+func CargarPuertoV2() (string, error) {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		return port, fmt.Errorf("no existe el puerto")
+	}
+	return port, nil
+}
+
+func EndpointsVehiculo(r *mux.Router)  {
 	//HANDLERS VEHICULO
 	r.HandleFunc("/vehiculos", rutas.GetVehiculosHandler).Methods("GET")
 	r.HandleFunc("/vehiculos/{id}", rutas.GetVehiculoHandler).Methods("GET")
@@ -45,16 +75,16 @@ func EndpointsProveedores(r *mux.Router) {
 
 }
 
-func EndpointsUsuarios(r *mux.Router) {
+func EndpointsUsuarios(r *mux.Router){
 
 	r.HandleFunc("/usuarios", rutasUsuarios.GetUsuariosHandler).Methods("GET")
-	r.HandleFunc("/usuarios/{username}", rutasUsuarios.GetUsuarioByIdHandler).Methods("GET")
+    r.HandleFunc("/usuarios/{username}", rutasUsuarios.GetUsuarioByIdHandler).Methods("GET")
 	r.HandleFunc("/usuarios/roles/{rol}", rutasUsuarios.GetUsuariosByRolHandler).Methods("GET")
-
+	
 	r.HandleFunc("/usuarios/{username}", rutasUsuarios.EditarUsuario).Methods("PUT") //Modificar datos de algun usuario
-	r.HandleFunc("/usuarios", rutasUsuarios.CrearUsuario).Methods("POST")            //crear un usuario
+	r.HandleFunc("/usuarios", rutasUsuarios.CrearUsuario).Methods("POST") //crear un usuario
 	r.HandleFunc("/usuarios/login", rutasUsuarios.Loguearse).Methods("POST")
 
-	r.HandleFunc("/usuarios/{username}", rutasUsuarios.EliminarUsuario).Methods("DELETE")
-
+	r.HandleFunc("/usuarios/{username}",rutasUsuarios.EliminarUsuario).Methods("DELETE")
+	
 }
