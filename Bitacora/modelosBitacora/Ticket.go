c8efc33c1c3b56a861bcefb391c68ec58e61e185
Melina Britos <melinal.britos@gmail.com>
2024-10-11 00:54:09 -0300

    Branch vehiculo (#12)
    * validaciones
    
    * Ticket
    
    * Marcas y modelos

---
+++ Bitacora/modelosBitacora/Ticket.go
@@ -0,0 +1,19 @@
+package modelosBitacora
+
+import (
+	"gorm.io/gorm"
+)
+
+type Ticket struct {
+	gorm.Model
+
+	Username              string `gorm:"not null"`
+	DescripcionProblema   string `gorm:"not null"`
+	FechaCreacion         string `gorm:"not null"`
+	Estado                string `gorm:"not null"`
+	Tipo                  string `gorm:"not null"`
+	Matricula             string `gorm:"not null"`
+	CostoTotal            float32
+	DescripcionReparacion string
+	FechaFinalizacion     string
+}
