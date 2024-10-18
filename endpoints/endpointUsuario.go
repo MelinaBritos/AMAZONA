a7c2c2dfe6d997b3fc880dea7add5ee1685ac4f5
MAG <gabriellimiguel01@gmail.com>
2024-10-18 17:47:44 -0300

    agregado el delete many y el create many

--- endpoints/endpointUsuario.go
+++ endpoints/endpointUsuario.go
@@ -11,9 +11,13 @@ func EndpointsUsuario(r *mux.Router) {
 	r.HandleFunc("/usuarios/{username}", rutasUsuario.GetUsuarioByUsernameHandler).Methods("GET")
 	r.HandleFunc("/usuarios/roles/{rol}", rutasUsuario.GetUsuariosByRolHandler).Methods("GET")
 
-	r.HandleFunc("/usuarios/{username}", rutasUsuario.EditarUsuario).Methods("PUT") //Modificar datos de algun usuario
-	r.HandleFunc("/usuarios", rutasUsuario.CrearUsuario).Methods("POST")            //crear un usuario
+	r.HandleFunc("/usuarios/{username}", rutasUsuario.EditarUsuario).Methods("PUT") 
+
+	r.HandleFunc("/usuarios/create", rutasUsuario.CrearUsuario).Methods("POST")            
+	r.HandleFunc("/usuarios/createMany", rutasUsuario.CrearUsuarios).Methods("POST")
+
 	r.HandleFunc("/usuarios/login", rutasUsuario.Loguearse).Methods("POST")
 
 	r.HandleFunc("/usuarios/{username}", rutasUsuario.EliminarUsuario).Methods("DELETE")
+	r.HandleFunc("/usuarios/delete/deleteMany", rutasUsuario.EliminarUsuarios).Methods("DELETE")
 }
