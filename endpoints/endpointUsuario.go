492a4a32d870529582c6a7485bd42f67dfda46e4
MAG <gabriellimiguel01@gmail.com>
2024-11-11 11:26:15 -0300

    agregado el deshabilitar usuario

--- endpoints/endpointUsuario.go
+++ endpoints/endpointUsuario.go
@@ -1,24 +1,27 @@
 package endpoints
 
 import (
-	"github.com/MelinaBritos/TP-Principal-AMAZONA/Usuario/rutasUsuario"
+	UserService "github.com/MelinaBritos/TP-Principal-AMAZONA/Usuario/rutasUsuario"
 	"github.com/gorilla/mux"
 )
 
+
+
 func EndpointsUsuario(r *mux.Router) {
 
-	r.HandleFunc("/usuarios", rutasUsuario.GetUsuariosHandler).Methods("GET")
-	r.HandleFunc("/usuarios/{username}", rutasUsuario.GetUsuarioByUsernameHandler).Methods("GET")
-	r.HandleFunc("/usuarios/roles/{rol}", rutasUsuario.GetUsuariosByRolHandler).Methods("GET")
+	r.HandleFunc("/usuarios", UserService.GetUsuariosHandler).Methods("GET")
+	r.HandleFunc("/usuarios/{username}", UserService.GetByUsername).Methods("GET")
+	r.HandleFunc("/usuarios/roles/{rol}", UserService.GetByRol).Methods("GET")
 
-	r.HandleFunc("/usuarios/{username}", rutasUsuario.EditarUsuario).Methods("PUT") 
-	r.HandleFunc("/usuarios/modify/updateMany", rutasUsuario.EditarUsuarios).Methods("PUT")
+	r.HandleFunc("/usuarios/{username}", UserService.Editar).Methods("PUT") 
+	r.HandleFunc("/usuarios/modify/updateMany", UserService.EditMany).Methods("PUT")
 
-	r.HandleFunc("/usuarios/create", rutasUsuario.CrearUsuario).Methods("POST")            
-	r.HandleFunc("/usuarios/create/Many", rutasUsuario.CrearUsuarios).Methods("POST")
+	r.HandleFunc("/usuarios/create", UserService.Crear).Methods("POST")            
+	r.HandleFunc("/usuarios/create/Many", UserService.CreateMany).Methods("POST")
 
-	r.HandleFunc("/usuarios/login", rutasUsuario.Loguearse).Methods("POST")
+	r.HandleFunc("/usuarios/login", UserService.Loguearse).Methods("POST")
 
-	r.HandleFunc("/usuarios/{username}", rutasUsuario.EliminarUsuario).Methods("DELETE")
-	r.HandleFunc("/usuarios/delete/deleteMany", rutasUsuario.EliminarUsuarios).Methods("DELETE")
+	r.HandleFunc("/usuarios/{username}", UserService.Eliminar).Methods("DELETE")
+	r.HandleFunc("/usuarios/deshabilitar/{username}", UserService.Deshabilitar).Methods("DELETE")
+	r.HandleFunc("/usuarios/delete/deleteMany", UserService.EliminarMuchos).Methods("DELETE")
 }
