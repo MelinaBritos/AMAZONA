aac0a4444d2513dd43cad3420b41d1d47aa8475a
MAG <gabriellimiguel01@gmail.com>
2024-11-12 12:03:56 -0300

    arreglos de usuario

--- endpoints/endpointUsuario.go
+++ endpoints/endpointUsuario.go
@@ -9,19 +9,32 @@ import (
 
 func EndpointsUsuario(r *mux.Router) {
 
-	r.HandleFunc("/usuarios", UserService.GetUsuariosHandler).Methods("GET")
-	r.HandleFunc("/usuarios/{username}", UserService.GetByUsername).Methods("GET")
-	r.HandleFunc("/usuarios/roles/{rol}", UserService.GetByRol).Methods("GET")
+	Get(r)
+	Put(r)
+	Post(r)
+	Delete(r)
+}
 
-	r.HandleFunc("/usuarios/{username}", UserService.Editar).Methods("PUT") 
-	r.HandleFunc("/usuarios/modify/updateMany", UserService.EditMany).Methods("PUT")
-
-	r.HandleFunc("/usuarios/create", UserService.Crear).Methods("POST")            
-	r.HandleFunc("/usuarios/create/Many", UserService.CreateMany).Methods("POST")
-
-	r.HandleFunc("/usuarios/login", UserService.Loguearse).Methods("POST")
-
+func Delete(r *mux.Router) {
 	r.HandleFunc("/usuarios/{username}", UserService.Eliminar).Methods("DELETE")
 	r.HandleFunc("/usuarios/deshabilitar/{username}", UserService.Deshabilitar).Methods("DELETE")
 	r.HandleFunc("/usuarios/delete/deleteMany", UserService.EliminarMuchos).Methods("DELETE")
+}
+
+func Post(r *mux.Router) {
+	r.HandleFunc("/usuarios/create", UserService.Crear).Methods("POST")
+	r.HandleFunc("/usuarios/create/Many", UserService.CreateMany).Methods("POST")
+
+	r.HandleFunc("/usuarios/login", UserService.Loguearse).Methods("POST")
+}
+
+func Put(r *mux.Router) {
+	r.HandleFunc("/usuarios/{username}", UserService.Editar).Methods("PUT")
+	r.HandleFunc("/usuarios/modify/updateMany", UserService.EditMany).Methods("PUT")
+}
+
+func Get(r *mux.Router) {
+	r.HandleFunc("/usuarios", UserService.GetUsuariosHandler).Methods("GET")
+	r.HandleFunc("/usuarios/{username}", UserService.GetByUsername).Methods("GET")
+	r.HandleFunc("/usuarios/roles/{rol}", UserService.GetByRol).Methods("GET")
 }
